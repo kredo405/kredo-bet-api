77c9d5c9b33ad664c44ea6373a43a3e1b78d4a6d
kredo405 <63173131+kredo405@users.noreply.github.com>
2024-12-27 22:56:12 +0300

    fix bug

--- firebase.js
+++ firebase.js
@@ -1,25 +1,25 @@
 // firebase.js
-import admin from "firebase-admin";
-import * as fs from "fs"; // Import the file system module
+// import admin from "firebase-admin";
+// import * as fs from "fs"; // Import the file system module
 
-// Read the JSON file synchronously
-const serviceAccount = JSON.parse(
-  fs.readFileSync(
-    "./kredo-bet-firebase-adminsdk-n4673-b3eab19e39.json",
-    "utf-8"
-  )
-);
+// // Read the JSON file synchronously
+// const serviceAccount = JSON.parse(
+//   fs.readFileSync(
+//     "./kredo-bet-firebase-adminsdk-n4673-b3eab19e39.json",
+//     "utf-8"
+//   )
+// );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-// Remove the duplicate initialization
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
 // });
 
-import { getFirestore } from "firebase-admin/firestore";
-const db = getFirestore();
+// // Remove the duplicate initialization
+// // admin.initializeApp({
+// //   credential: admin.credential.cert(serviceAccount),
+// // });
 
-export default db;
+// import { getFirestore } from "firebase-admin/firestore";
+// const db = getFirestore();
+
+// export default db;
